9463c8e3740c99aacc8a7b3c606fa3ead35c9eaa
Igor <pilot.igor97@gmail.com>
2026-05-08 12:25:53 -0300

    build(app): sync Flutter — UX 3 tipos resource-exhausted

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2fb07aea43ef44ef6a8f9d993bc011c4",
+const RESOURCES = {"flutter_bootstrap.js": "80b94d1cf0b6507ede2409051a52fa87",
 "version.json": "913deafd0d3544e67487bc96d57dd160",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "47ee98331fa30b6174d2bc7d15b18909",
+"main.dart.js": "5367bb25773b7da9f044c07c3904a9f6",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "60e21d697a047a4aa2752c932d4f8af6",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
