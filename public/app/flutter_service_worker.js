43a4bae575417c85408c8cb98b226b318fa43412
Igor <pilot.igor97@gmail.com>
2026-05-11 20:45:29 -0300

    build(app): mostrar tickets sistema-gerados por default (alinha legacy)

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a0a2b768e6d822998760b5a7ab352cdf",
+const RESOURCES = {"flutter_bootstrap.js": "0452c811cd87d353236ccbc9ca83144f",
 "version.json": "913deafd0d3544e67487bc96d57dd160",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "58c74db538a740792921598a682cf705",
+"main.dart.js": "caf4be10561a8dfb37915b8fe23641c5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "60e21d697a047a4aa2752c932d4f8af6",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
