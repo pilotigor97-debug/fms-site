1e96876c29457843a5440ae0a0e5a1cca1c7a247
Igor <pilot.igor97@gmail.com>
2026-05-13 20:28:03 -0300

    chore(web): rebuild Flutter — datas com afford + criar equip inline

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2651968594"
+    serviceWorkerVersion: "1558534893"
   }
 });
