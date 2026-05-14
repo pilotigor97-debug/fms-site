fbec162859840d8699c15f1606150e527b4266dc
Igor <pilot.igor97@gmail.com>
2026-05-14 00:16:27 -0300

    chore(web): rebuild — UX Fase 4 (Recent Items no Cmd+K)

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8d05cd02411e5d2e6b4470c046842738",
+const RESOURCES = {"flutter_bootstrap.js": "988cca2149bc9d5665c8aa0ce8286c78",
 "version.json": "9cd9af1686a007d8b73792df6a84c3cc",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "7e1db22644f99579e980738b363504ae",
+"main.dart.js": "66cd6cc28b07210e3801eb246a0a7040",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9744969c8dca56ea0887b445ad1ce54f",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
