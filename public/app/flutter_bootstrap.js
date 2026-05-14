7cfc77a58bc1b45194d5fa0194d20ea5381d449e
Igor <pilot.igor97@gmail.com>
2026-05-14 12:06:17 -0300

    chore(web): rebuild — FAB v2 (Positioned absoluto, hit testing OK)

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2165211048"
+    serviceWorkerVersion: "2991078501"
   }
 });
