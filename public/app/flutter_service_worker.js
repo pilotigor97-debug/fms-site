e0e57835d5107bfcbb5663d813123361b6d9e18d
Igor <pilot.igor97@gmail.com>
2026-05-13 22:35:41 -0300

    chore(web): rebuild Flutter — OS avulsa + auto-criar chamado

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6db83c7e7c6d09f17514f33f02e8b6ce",
+const RESOURCES = {"flutter_bootstrap.js": "8f8024fc40587554af50e8e8fbf6d051",
 "version.json": "bcdbe4d4f0e8408593ffe4694b890c05",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "676add3f4a8c8d5bb2507518a26544c8",
+"main.dart.js": "56e36f4c44c910e3d50390bd73bb7d50",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9744969c8dca56ea0887b445ad1ce54f",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
