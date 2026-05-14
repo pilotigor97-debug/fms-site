ddd692994f7953de73184ab568c946b7b5fc41a4
Igor <pilot.igor97@gmail.com>
2026-05-14 12:29:41 -0300

    chore(web): rebuild — FAB v3 fix Positioned

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "44a0d377fed7ef6d7101083a78806489",
+const RESOURCES = {"flutter_bootstrap.js": "5a74a966d3f46bb1e579d3be5b1fa6c7",
 "version.json": "9cd9af1686a007d8b73792df6a84c3cc",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "dd00bb12e62138738cf7eccab383a74c",
+"main.dart.js": "120ad32258d94658a4abd6f780f1c744",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9744969c8dca56ea0887b445ad1ce54f",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
