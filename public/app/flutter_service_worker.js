c44e1c93f7c26f872e22152ac0e15bd2747cda19
Igor <pilot.igor97@gmail.com>
2026-05-14 14:10:24 -0300

    chore(web): rebuild — manuais dinâmicos com cache offline

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e529203a97075b10c3d678712a55a3a8",
+const RESOURCES = {"flutter_bootstrap.js": "7271a064f666543402aa219bd14b680d",
 "version.json": "9cd9af1686a007d8b73792df6a84c3cc",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "acd357acb09451d4d587b01636cb1192",
+"main.dart.js": "9ce00aec15b1ab7c8a00b83f9d7159a1",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9744969c8dca56ea0887b445ad1ce54f",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
