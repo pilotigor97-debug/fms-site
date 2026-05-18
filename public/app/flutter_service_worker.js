0b56c775a06761d8f3e6fb0f919fef2d32c2971b
Igor <pilot.igor97@gmail.com>
2026-05-17 22:33:56 -0300

    chore(web): rebuild — label rental dispatch=Agenda de Técnicos

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "19380a633a2363bd987ff427a7ec1b20",
+const RESOURCES = {"flutter_bootstrap.js": "785410df369178188709a7ba20392750",
 "version.json": "9cd9af1686a007d8b73792df6a84c3cc",
 "index.html": "7c712f6747b13e7d95c6a4f622cc2594",
 "/": "7c712f6747b13e7d95c6a4f622cc2594",
 "baixar.html": "c0f2b2212e2a64ca0d98b960bcf52508",
 "orcamento.html": "fce52e3ee51cc652bb88de9fdc5b70db",
-"main.dart.js": "821f8d7bae9c55d4b83aae026cb48516",
+"main.dart.js": "c2ce3eb31e2c369d0dab6f8971e05a26",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9744969c8dca56ea0887b445ad1ce54f",
 "download/version.json": "cb54a1f93710df3fea38b058e295779d",
